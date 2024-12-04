066a2f5967c297c1838c7497c4a2c8a3b0a8a6b1
Hemanth <hemanthk235@gmail.com>
2024-12-04 11:49:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "us-east-2"
 }
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-088d38b423bff245f"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "hemanth"
+vpc_security_group_ids = ["sg-0de17257dfe5b6e7e"]
 tags = {
 Name = var.instance_names[count.index]
 }
